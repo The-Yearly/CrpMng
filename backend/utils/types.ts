419b50b6b1a99c222e133f07a261c693c5a1ddee
The-Yearly <theyearlysclub@gmail.com>
2026-01-28 19:04:36 +0530

    Added Docker File

---
+++ backend/utils/types.ts
@@ -0,0 +1,37 @@
+import { LatLngTuple } from "leaflet";
+export interface plotType {
+  agentId: number;
+  plotId: number;
+  farmerId: number;
+  crop: string;
+  location: string;
+  plot: LatLngTuple[];
+  plotImage: string;
+}
+
+export interface farmerType {
+  agentId: number;
+  farmerId: number;
+  name: string;
+  noOfPlots: number;
+  farmerImage: string;
+  phone: string;
+}
+
+export interface AgentType {
+  agentId: number;
+  agentPic: string;
+  agentName: string;
+  agentMail: string;
+  agentPass: string;
+}
+export interface selectedPlot extends plotType, farmerType {}
+
+export interface PlotDets extends plotType {
+  fid: farmerType;
+}
+
+export interface fullFarmerDetails extends farmerType {
+  crops: string[];
+  locations: string[];
+}
